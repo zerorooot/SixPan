89f6d428f28ed19b7553b07d194d4d721cefe2d4
Wang <msj1013654754@gmail.com>
2020-11-03 22:32:00 +0800

    更新api网址

--- src/main/java/com/github/zerorooot/bean/ApiUrl.java
+++ src/main/java/com/github/zerorooot/bean/ApiUrl.java
@@ -5,18 +5,18 @@ package com.github.zerorooot.bean;
  * @Date: 2020/8/7 19:51
  */
 public class ApiUrl {
-    public static final String DELETE = "https://api.6pan.cn/v3/newfile/trash";
-    public static final String LIST = "https://api.6pan.cn/v3/newfile/list";
-    public static final String RENAME = "https://api.6pan.cn/v3/file/rename";
-    public static final String LOGIN = "https://account.6pan.cn/v3/oauth/login";
-    public static final String CHECKCOOKIE = "https://account.6pan.cn/v3/oauth/checkCookie?appid=bc088aa5e2ad&destination=https:%2F%2Fv3-beta.6pan.cn%2Ffiles%2Fall%2F";
-    public static final String MOVE = "https://api.6pan.cn/v3/file/move";
-    public static final String CREATEFOLDER = "https://api.6pan.cn/v3/newfile";
-    public static final String DOWNLOAD = "https://api.6pan.cn/v3/newfile/download";
-    public static final String QUOTA = "https://api.6pan.cn/v3/offline/quota";
-    public static final String PARSE = "https://api.6pan.cn/v3/offline/parse";
-    public static final String ADDOFFLINE = "https://api.6pan.cn/v3/offline/add";
-    public static final String OFFLINELIST = "https://api.6pan.cn/v3/offline/list";
-    public static final String DELETECOMPLETE = "https://api.6pan.cn/v3/offline/clear";
-    public static final String OFFLINEDELETE = "https://api.6pan.cn/v3/offline/delete";
+    public static final String DELETE = "https://api.2dland.cn/v3/newfile/trash";
+    public static final String LIST = "https://api.2dland.cn/v3/newfile/list";
+    public static final String RENAME = "https://api.2dland.cn/v3/file/rename";
+    public static final String LOGIN = "https://account.2dland.cn/v3/oauth/login";
+    public static final String CHECKCOOKIE = "https://account.2dland.cn/v3/oauth/checkCookie?appid=bc088aa5e2ad&destination=https:%2F%2Fv3-beta.2dland.cn%2Ffiles%2Fall%2F";
+    public static final String MOVE = "https://api.2dland.cn/v3/file/move";
+    public static final String CREATEFOLDER = "https://api.2dland.cn/v3/newfile";
+    public static final String DOWNLOAD = "https://api.2dland.cn/v3/newfile/download";
+    public static final String QUOTA = "https://api.2dland.cn/v3/offline/quota";
+    public static final String PARSE = "https://api.2dland.cn/v3/offline/parse";
+    public static final String ADDOFFLINE = "https://api.2dland.cn/v3/offline/add";
+    public static final String OFFLINELIST = "https://api.2dland.cn/v3/offline/list";
+    public static final String DELETECOMPLETE = "https://api.2dland.cn/v3/offline/clear";
+    public static final String OFFLINEDELETE = "https://api.2dland.cn/v3/offline/delete";
 }
